0ecbb3db14ec7e34380d2c16f150f411bfbeabe6
s1ash <85079286+s1ashxd@users.noreply.github.com>
2023-04-04 01:09:38 +0300

    Add mod data contants

--- src/main/kotlin/net/fabricmc/example/ExampleMod.kt
+++ src/main/kotlin/net/fabricmc/example/ExampleMod.kt
@@ -1,6 +1,22 @@
 @file:Suppress("unused")
 package net.fabricmc.example
 
+import net.fabricmc.loader.api.FabricLoader
+import org.slf4j.LoggerFactory
+import kotlin.io.path.absolutePathString
+
+@JvmField
+val MOD_ID = "modid"
+
+@JvmField
+val METADATA = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().metadata
+
+@JvmField
+val FOLDER = FabricLoader.getInstance().gameDir.resolve(MOD_ID).absolutePathString()
+
+@JvmField
+val LOGGER = LoggerFactory.getLogger(MOD_ID)
+
 fun init() {
     println("Initializing")
 }
